c8a02212dcee0708fba208ac8229ce5d7e641011
Lukas <lukashuwald@online.de>
2017-09-13 17:31:58 +0200

    removed test_load

--- preprocess/test_load.py
+++
@@ -1,9 +0,0 @@
-import tensorflow as tf
-import numpy as np
-
-sess = tf.Session()
-loaded = np.load('small_bitmaps/1002/0_320x180.npy')
-png = tf.image.encode_png(loaded)
-pic = sess.run(png)
-with open('test.png', 'wb') as output:
-    output.write(pic)
